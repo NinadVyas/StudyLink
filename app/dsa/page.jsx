fef1b73d7a16705bcb8a36a4650de3362e6cf2ef
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-11-15 22:34:30 +0530

    made changes

--- app/dsa/page.jsx
+++ app/dsa/page.jsx
@@ -1,15 +1,7 @@
 "use client"
 import { Container } from "/components/container";
-import { LockIcon, UsersIcon, ChartIcon, WalletIcon } from "/components/icons";
 import styles from "/styles/index.module.scss";
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
@@ -18,17 +10,29 @@ import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import PrintIcon from '@mui/icons-material/Print';
 import ShareIcon from '@mui/icons-material/Share';
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Ninad Vyas', 20, 6),
-  ];
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab, { tabClasses } from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
+import Typography from '@mui/joy/Typography';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import TopicIcon from '@mui/icons-material/Topic';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import LayersIcon from '@mui/icons-material/Layers';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import StarIcon from '@mui/icons-material/Star';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
   const actions = [
-    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <FileCopyIcon />, name: 'Algo101' },
     { icon: <SaveIcon />, name: 'Save' },
     { icon: <PrintIcon />, name: 'Print' },
     { icon: <ShareIcon />, name: 'Share' },
@@ -38,38 +42,182 @@ export default function Course() {
 const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [index, setIndex] = React.useState(0);
+  const colors = ['primary', 'danger', 'success', 'warning'];
 
   return (
     <Container title="Resource">
+      <Box
+      sx={{
+        flexGrow: 1,
+        m: -3,
+        p: 4,
+        borderTopLeftRadius: '12px',
+        borderTopRightRadius: '12px',
+        bgcolor: 'primary',
+      }}
+    >
+      <Tabs
+        size="lg"
+        aria-label="Bottom Navigation"
+        value={index}
+        onChange={(event, value) => setIndex(value)}
+        sx={(theme) => ({
+          p: 1,
+          borderRadius: 16,
+          maxWidth: 400,
+          mx: 'auto',
+          boxShadow: theme.shadow.sm,
+          '--joy-shadowChannel': theme.vars.palette[colors[index]].darkChannel,
+          [`& .${tabClasses.root}`]: {
+            py: 1,
+            flex: 1,
+            transition: '0.3s',
+            fontWeight: 'md',
+            fontSize: 'md',
+            [`&:not(.${tabClasses.selected}):not(:hover)`]: {
+              opacity: 0.7,
+            },
+          },
+        })}
+      >
+        <TabList
+          variant="plain"
+          size="sm"
+          disableUnderline
+          sx={{ borderRadius: 'lg', p: 0 }}
+        >
+          <Tab
+            disableIndicator
+            orientation="vertical"
+            {...(index === 0 && { color: "primary" })}
+          >
+            <ListItemDecorator>
+              <TopicIcon />
+            </ListItemDecorator>
+            Materials
+          </Tab>
+          <Tab
+            disableIndicator
+            orientation="vertical"
+            {...(index === 1 && { color: colors[1] })}
+          >
+            <ListItemDecorator>
+              <AutoStoriesIcon />
+            </ListItemDecorator>
+            Blogs
+          </Tab>
+          <Tab
+            disableIndicator
+            orientation="vertical"
+            {...(index === 2 && { color: colors[2] })}
+          >
+            <ListItemDecorator>
+              <LayersIcon />
+            </ListItemDecorator>
+            Roadmap
+          </Tab>
+          <Tab
+            disableIndicator
+            orientation="vertical"
+            {...(index === 3 && { color: colors[3] })}
+          >
+            <ListItemDecorator>
+              <FormatListBulletedIcon />
+            </ListItemDecorator>
+            Q&A
+          </Tab>
+        </TabList>
+      </Tabs>
+    </Box>
       <div >
-        <div className={styles.heading}>
-        
-          <div>DSA</div>
-          <TableContainer component={Paper} className={styles.tabledsa}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table" className={styles.tabledsa}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Docs</TableCell>
-            <TableCell align="right">Files</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <div>
+    <Tabs
+        size="lg"
+        aria-label="Bottom Navigation"
+        value={index}
+        onChange={(event, value) => setIndex(value)}
+        sx={(theme) => ({
+          p: 1,
+          borderRadius: 16,
+          maxWidth: 1000,
+          mx: 'auto',
+          boxShadow: theme.shadow.sm,
+          '--joy-shadowChannel': theme.vars.palette[colors[index]].darkChannel,
+          [`& .${tabClasses.root}`]: {
+            py: 1,
+            flex: 1,
+            transition: '0.3s',
+            fontWeight: 'md',
+            fontSize: 'md',
+            [`&:not(.${tabClasses.selected}):not(:hover)`]: {
+              opacity: 0.7,
+            },
+          },
+        })}
+    >
+      <TabList
+        disableUnderline
+        tabFlex={1}
+        sx={{
+          [`& .${tabClasses.root}`]: {
+            fontSize: 'sm',
+            fontWeight: 'lg',
+            [`&[aria-selected="true"]`]: {
+              color: 'primary.500',
+              bgcolor: 'background.surface',
+            },
+            [`&.${tabClasses.focusVisible}`]: {
+              outlineOffset: '-4px',
+            },
+          },
+        }}
+      >
+      </TabList>
+      <TabPanel value={0}>
+      <Card variant="outlined" sx={{ width: 320 }}>
+      <CardOverflow>
+        <AspectRatio ratio="2">
+          <img
+            src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
+            srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
+      </CardOverflow>
+      <CardContent>
+       <a href=''> <Typography level="title-md">Dsa Imp By Ninad Vyas</Typography></a>
+        <Typography level="body-sm">India</Typography>
+      </CardContent>
+    </Card>
+      </TabPanel>
+      <TabPanel value={1}>
+        <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      aria-label="contacts"
+    >
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary="Ninad Vyas" />
+        </ListItemButton>
+      </ListItem>
+    </List>
+      </TabPanel>
+      <TabPanel value={2}>
+        <Typography level="inherit">
+         Comming Soon!(Roadmap)
+        </Typography>
+      </TabPanel>
+      <TabPanel value={3}>
+        <Typography level="inherit">
+          Comming Soon!(Q&N)
+        </Typography>
+      </TabPanel>
+    </Tabs>
         </div>
         <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
       <SpeedDial
